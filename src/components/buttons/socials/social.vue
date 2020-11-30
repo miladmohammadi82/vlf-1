f3258befc98c65c28b72975987f8360f1b837b83
Amirhossein Fazli <amirhossein95b@gmail.com>
2020-11-30 18:36:39 +0330

    newsletter and footer

---
+++ src/components/buttons/socials/social.vue
@@ -0,0 +1,21 @@
+<template>
+    <Btn :to="link" class="social-btn" cr="rgba(255, 255, 255, 0.3)">
+        <i :class="[icon]"></i>
+    </Btn>
+</template>
+
+<script>
+    import Btn from "../btn";
+    export default {
+        name: "social",
+        props: {
+            link: {
+                default: false,
+            },
+            icon: {
+                type: String,
+            },
+        },
+        components: { Btn },
+    };
+</script>
---
+++ src/components/newsletter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="mt-5 pt-5 d-flex flex-column">
+        <i class="fas fa-envelope newsletter-icon"></i>
+        <p style="text-align: center;" class="mt-3 mb-4">عضو خبرنامه ما شوید تا از آخرین تخفیف ها و اطلاعیه ها جا نمانید !</p>
+        <form action="#" class="mx-auto d-flex align-items-center flex-column" style="width: 100%">
+            <input type="email" class="newsletter-inp col-lg-3" placeholder="ایمیل شما ..." />
+            <btn-sccss-out class="mt-4">عضویت در خبرنامه</btn-sccss-out>
+        </form>
+    </div>
+</template>
+
+<script>
+    import btnSccssOut from "./buttons/success/outline";
+    export default {
+        name: "newsletter",
+        components: {
+            btnSccssOut
+        }
+    };
+</script>
