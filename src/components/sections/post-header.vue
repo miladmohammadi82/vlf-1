3033247150c45b00bc43b49105ace619b17b514a
Amirhossein Fazli <amirhossein95b@gmail.com>
2020-12-26 17:18:54 +0330

    post header

---
+++ src/components/sections/post-header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <img :src="img" :alt="alt" class="post-img" />
+        <div class="post-author">
+            <a :href="author.link" class="author-image">
+                <img :src="author.avatar" :alt="author.name">
+            </a>
+            <ul>
+                <li><i class="far fa-eye"></i><span>540 بازدید</span></li>
+                <li><i class="far fa-calendar-alt"></i><span>1400/01/01</span></li>
+                <li><i class="fas fa-user"></i><a href="#">امیرحسین فضلی</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PostHeader",
+        props: {
+            img: {
+                type: String,
+                required: true,
+            },
+            author: {
+                type: Object,
+                required: true,
+            },
+            alt: {
+                type: String,
+                required: true,
+            },
+        },
+    };
+</script>
--- src/pages/post-single.vue
+++ src/pages/post-single.vue
@@ -4,6 +4,7 @@
         <div class="container-lg">
             <h1 class="mt-5 post-title">عنوان پست را چگونه بنویسیم؟</h1>
             <Breadcrumb class="mt-4" :items="items" :active="active" />
+            <PostHeader :img="require('../assets/img/posts/vue_js.jpg')" :author="author" alt="alt post" />
         </div>
         <Footer />
     </div>
@@ -13,12 +14,14 @@
     import Header from "../components/sections/header";
     import Footer from "../components/sections/footer";
     import Breadcrumb from "../components/sections/breadcrumb";
+    import PostHeader from "../components/sections/post-header";
     export default {
         name: "PostSingle",
         components: {
             Header,
             Footer,
             Breadcrumb,
+            PostHeader,
         },
         data() {
             return {
@@ -36,7 +39,12 @@
                         link: "#"
                     }
                 ],
-                active: "چگونه breadcrumb بسازیم"
+                active: "چگونه breadcrumb بسازیم",
+                author: {
+                    name: "Amirhossein",
+                    avatar: require('../assets/img/Amir-developer-avatar.jpg'),
+                    link: "#"
+                },
             };
         },
     };
